4f41a831be9b0f47edd51d9e443bc9df88a41bab
MKW420 <keitumetseshongwe@gmail.com>
2023-07-24 13:48:45 +0200

    SideBar Navigation Compnent created

---
+++ my-vue-app/src/router/index.js
@@ -0,0 +1,18 @@
+import {createRouter,createWebHistory} from 'vue-router'
+import App from '../views/App.vue'
+
+const routes = [
+    { 
+        path:'/',
+        name:'App',
+        component:App
+    },
+  
+];
+
+const router = createRouter({
+    history:createWebHistory(process.env.BASE_URL),
+    routes,
+});
+
+export default router;
